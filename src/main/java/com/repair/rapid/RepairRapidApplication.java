239b602a68dab6bec65320e68a2eb20c78cd5399
piyanshu <piyanshumangla456.com>
2024-03-12 21:51:56 +0530

    My First Commit

---
+++ src/main/java/com/repair/rapid/RepairRapidApplication.java
@@ -0,0 +1,13 @@
+package com.repair.rapid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairRapidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairRapidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repair/rapid/RepairRapidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repair.rapid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairRapidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

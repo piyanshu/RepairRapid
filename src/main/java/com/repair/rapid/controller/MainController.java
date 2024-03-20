09020ef57d71a4e3af1baf4f4abe30f522467004
piyanshu <piyanshumangla456.com>
2024-03-20 23:09:11 +0530

    making a new branch and remove .target file --piyanshu

--- src/main/java/com/repair/rapid/controller/MainController.java
+++ src/main/java/com/repair/rapid/controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	@RequestMapping("/")
+	@RequestMapping("/home")
 	public String home() {
 		return "home";
 	}
